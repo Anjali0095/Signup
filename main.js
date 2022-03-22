6db03b3057d925e4db4d021af447db0c657d5787
Anjali <smoni6465@gmail.com>
2022-03-22 12:07:06 +0530

    Javascript functinality added

--- main.js
+++ main.js
@@ -1,11 +1,43 @@
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
-​
-signUpButton.addEventListener('click', () => {
-    container.classList.add("right-panel-active");
+const signUpButton = document.getElementById("signUp");
+const signInButton = document.getElementById("signIn");
+
+const container = document.getElementById("container");
+
+signUpButton.addEventListener("click", () => {
+  container.classList.add("right-panel-active");
 });
-​
-signInButton.addEventListener('click', () => {
-    container.classList.remove("right-panel-active");
+
+signInButton.addEventListener("click", () => {
+  container.classList.remove("right-panel-active");
 });
+element.addEventListener("click", myFunction);
+
+function myFunction() {
+  let email = document.getElementById("email");
+  let password = document.getElementById("password");
+
+  //To check values not empty
+  if (email.value == "" || password.value == "") {
+    return Swal.fire("Error!", "All Fields are Required!", "error");
+  }
+
+  //login
+  if (email.value == "admin@gmail.com") {
+    if (password.value == "qawsed") {
+      return Swal.fire(
+        "Successfully!",
+        "You Successfully Signed In",
+        "success"
+      );
+    }
+    return Swal.fire("Error!", "Password is Wrong", "error");
+  } else {
+    //console.log("err");
+    Swal.fire("Error!", "Please Check Email and Password!", "error");
+  }
+}
+
+function s() {
+  console.log("hi");
+  Swal.fire("Error!", "All Fields are Required!", "error");
+}
